4d286bb2a994ef7016b582aef0b08dee41edeaad
Marcin Michniak <marcin.michniak@fortedigital.no>
2018-09-20 15:27:48 +0200

    Add state in board setup. Update private methods to access the state.

--- src/Components/BoardSetupComponent/BoardSetup.tsx
+++ src/Components/BoardSetupComponent/BoardSetup.tsx
@@ -2,19 +2,31 @@ import * as React from 'react';
 import { BoardDimensionForm } from '../BoardDimensionFormComponent/BoardDimensionForm';
 import './BoardSetup.css';
 
-export class BoardSetup extends React.Component {
-    public render(){
+interface IState {
+    width: number,
+    height: number
+}
+
+export class BoardSetup extends React.Component<{}, IState> {
+    public componentDidMount () {
+        this.state = {
+            width: 3,
+            height: 3
+        };
+    }
+
+    public render () {
         return (
             <div className="board-setup">
                 <h1>Setup</h1>
                 <div className="setup-box">
                     <div className="form-group">
                         <h3>Board width:</h3>
-                        <BoardDimensionForm handler={this.updateBoardXDimension} initialSize={2} />
+                        <BoardDimensionForm handler={this.updateBoardXDimension} initialSize={3} />
                     </div>
                     <div className="form-group">
                         <h3>Board height:</h3>
-                        <BoardDimensionForm handler={this.updateBoardYDimension} initialSize={2} />
+                        <BoardDimensionForm handler={this.updateBoardYDimension} initialSize={3} />
                     </div>
                     <button className="go-button">Go</button>
                 </div>
@@ -22,11 +34,7 @@ export class BoardSetup extends React.Component {
         );
     }
 
-    private updateBoardXDimension(dimension: number): void {
-        console.log(dimension);
-    }
+    private updateBoardXDimension = (dimension: number): void => this.setState({width: dimension});
 
-    private updateBoardYDimension(dimension: number): void {
-        console.log(dimension);
-    }
+    private updateBoardYDimension = (dimension: number): void => this.setState({height: dimension});
 }

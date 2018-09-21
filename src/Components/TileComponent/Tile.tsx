384948ab36a8edd18b789ff2a9435e0fc50be857
Marcin Michniak <marcin.michniak@fortedigital.no>
2018-09-21 14:31:48 +0200

    Set tile background color programmatically.

--- src/Components/TileComponent/Tile.tsx
+++ src/Components/TileComponent/Tile.tsx
@@ -8,14 +8,34 @@ interface IProps {
 }
 
 interface IState {
-    hexBackgroundColor: string,
+    backgroundColor: IRgbColor
+}
+
+interface IRgbColor{
+    r: number,
+    g: number,
+    b: number
 }
 
 export class Tile extends React.Component<IProps, IState> {
+    constructor (props: IProps) {
+        super(props);
+        const tileColor: IRgbColor = {
+            r: 30,
+            g: 144,
+            b: 255
+        };
+        this.state = {
+            backgroundColor: tileColor
+        };
+    }
+
     public render () {
+        const tileColor = this.state.backgroundColor; 
         const tileStyle = {
             flex: `1 1 calc(${this.props.width}% - 10px)`,
-            fontSize: this.props.width * 15 + "%"
+            fontSize: this.props.width * 15 + "%",
+            backgroundColor: `rgb(${tileColor.r},${tileColor.g},${tileColor.b})`
         };
         return (
             <div className="board-tile" style={tileStyle} onClick={this.onTileClick}>
